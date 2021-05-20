9d26a2fbf50c4782651033a0944a9a146cfc5c33
Bars <barsbekclash@gmail.com>
2021-05-20 13:58:03 +0600

    add validation in APIs

--- api/webapp/views.py
+++ api/webapp/views.py
@@ -3,60 +3,112 @@ from django.shortcuts import render
 # Create your views here.
 import json
 from datetime import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import math
 
 def add(request, *args, **kwargs):
-    cif = json.loads(request.body)
+    cif = request.body
     if cif:
-        if cif['A'] and cif['B']:
-            summ = cif['A'] + cif['B']
+        cif1 = json.loads(cif)
+        if isinstance(cif1['A'], int) and isinstance(cif1['B'], int):
+            summ = cif1['A'] + cif1['B']
             response_data = {
-                'result': str(summ),
-                'method': request.method,
+                'answer': str(summ),
             }
-    response = HttpResponse(json.dumps(response_data))
-    response['Content_Type'] = 'application/json'
+            response = JsonResponse(response_data)
+        else :
+            response_data = {
+                'answer' : 'Enter only whole numbers!!'
+            }
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    else:
+        response_data = {
+            'answer': 'Not Json!!'
+        }
+        response = JsonResponse(response_data)
+        response.status_code = 400
     return response
 
 
+
 def subtract(request, *args, **kwargs):
-    cif = json.loads(request.body)
+    cif = request.body
     if cif:
-        if cif['A'] and cif['B']:
-            subtract = cif['A'] - cif['B']
+        cif1 = json.loads(cif)
+        if isinstance(cif1['A'], int) and isinstance(cif1['B'], int):
+            subtract = cif1['A'] - cif1['B']
             response_data = {
-                'result': str(subtract),
-                'method': request.method,
+                'answer': str(subtract),
             }
-    response = HttpResponse(json.dumps(response_data))
-    response['Content_Type'] = 'application/json'
+            response = JsonResponse(response_data)
+        else :
+            response_data = {
+                'answer' : 'Enter only whole numbers!!'
+            }
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    else:
+        response_data = {
+            'answer': 'Not Json!!'
+        }
+        response = JsonResponse(response_data)
+        response.status_code = 400
     return response
 
 
 def multiply(request, *args, **kwargs):
-    cif = json.loads(request.body)
+    cif = request.body
     if cif:
-        if cif['A'] and cif['B']:
-            multiply = cif['A'] * cif['B']
+        cif1 = json.loads(cif)
+        if isinstance(cif1['A'], int) and isinstance(cif1['B'], int):
+            multiply = cif1['A'] * cif1['B']
             response_data = {
-                'result': str(multiply),
-                'method': request.method,
+                'answer': str(multiply),
             }
-    response = HttpResponse(json.dumps(response_data))
-    response['Content_Type'] = 'application/json'
+            response = JsonResponse(response_data)
+        else :
+            response_data = {
+                'answer' : 'Enter only whole numbers!!'
+            }
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    else:
+        response_data = {
+            'answer': 'Not Json!!'
+        }
+        response = JsonResponse(response_data)
+        response.status_code = 400
     return response
 
 
 def divide(request, *args, **kwargs):
-    cif = json.loads(request.body)
+    cif = request.body
     if cif:
-        if cif['A'] and cif['B']:
-            divide = cif['A'] / cif['B']
+        cif1 = json.loads(cif)
+        if isinstance(cif1['A'], int) and isinstance(cif1['B'], int):
+            if cif1['B'] == 0:
+                response_data = {
+                    'answer': 'Division by zero!!!'
+                }
+                response = JsonResponse(response_data)
+                response.status_code = 400
+            else:
+                divide = cif1['A'] / cif1['B']
+                response_data = {
+                    'answer': str(divide),
+                }
+                response = JsonResponse(response_data)
+        else :
             response_data = {
-                'result': str(divide),
-                'method': request.method,
+                'answer' : 'Enter only whole numbers!!'
             }
-    response = HttpResponse(json.dumps(response_data))
-    response['Content_Type'] = 'application/json'
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    else:
+        response_data = {
+            'answer': 'Not Json!!'
+        }
+        response = JsonResponse(response_data)
+        response.status_code = 400
     return response
